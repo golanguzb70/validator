fce10c1dd0bd0bcaaca50ea3782e2cdacfb25359
Golang Uzbekistan <golanguzb70@gmail.com>
2023-06-20 17:32:56 +0500

    fix function names

--- vaildator.go
+++ vaildator.go
@@ -9,7 +9,7 @@ import (
 )
 
 // Funksiya uuid ni valid yoki yo'qligini tekshiradi
-func IsUUIDValid(id string) bool {
+func IsUUID(id string) bool {
 	_, err := uuid.Parse(id)
 	if err != nil {
 		return false
@@ -19,14 +19,14 @@ func IsUUIDValid(id string) bool {
 }
 
 // Funksiyaga yuborilgan url ni tekshiradi link bulgan holatda true aks holatda false qaytaradi
-func IsLinkValid(url string) bool {
+func IsUrl(url string) bool {
 	urlRegex := regexp.MustCompile(`^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})(\/[\w.-]*)*\/?$`)
 	result := urlRegex.MatchString(url)
 	return result
 }
 
 // Funksiyaga yuborilgan emailni haqiqatdan email yoki yo'qligini tekshiradi
-func IsEmailValid(email string) bool {
+func IsEmail(email string) bool {
 	parts := strings.Split(email, "@")
 	if len(parts) != 2 {
 		return false
@@ -37,23 +37,14 @@ func IsEmailValid(email string) bool {
 	} else {
 		return true
 	}
-
 }
 
 // Agar time o'zgaruvchi null bo'lsa funksiya true qaytaradi aks holda false
 func IsTimeNull(date1 time.Time) bool {
-	if date1.IsZero() {
-		return true
-	} else {
-		return false
-	}
+	return date1.IsZero()
 }
 
 // Funksiyaga kelgan string farmatidagi o'zgaruvchilarni to'g'riligini tekshiradi
-func IsStringValue(value string) bool {
-	if value != "" && value != "string" {
-		return true
-	} else {
-		return false
-	}
+func IsEmpty(value string) bool {
+	return value == ""
 }
